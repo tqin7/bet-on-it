d3840cfbfe88630cdd712b081cf4d9e894969880
Tian Qin <tianqin@airbears2-10-142-65-47.airbears2.1918.berkeley.edu>
2017-10-07 17:58:36 -0700

    new project

--- server/main.js
+++ server/main.js
@@ -1,10 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-Posts = new Mongo.Collection("posts");
-Events = new Mongo.Collection("events");
-Deposits = new Mongo.Collection('deposits');
-
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
