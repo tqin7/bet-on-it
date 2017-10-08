07513d8114bc36366cfffd6f8833fa9d689cfbd3
Ashley Nguyen <ashley.nguyen27@berkeley.edu>
2017-10-07 19:05:15 -0700

    nav bar

--- client/main.js
+++ client/main.js
@@ -6,31 +6,17 @@ import './main.html';
 Tasks = new Mongo.Collection('tasks');
 Events = new Mongo.Collection('events');
 
-Template.body.helpers({
-    tasks: function() {
-        return Tasks.find();
-    }, 
-});
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-Template.body.events({
-    'submit .new-task': function(event) {
-        console.log("new task submitted");
-        var body = event.target.body.value;
-        if (body == "" ) {
-            alert("Task cannot be empty");
-        } else {
-            console.log("new task inserting");
-            Tasks.insert({
-                body: body,
-                completed: false,
-                upvotes: 0,
-                downvotes: 0
-            });
-        }
-        return false;
-    },
-});
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-Template.task.events({
-
-});
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
