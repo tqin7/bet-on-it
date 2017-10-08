eacc34e52b7c39c52b61ba8a761521b98f225df8
Ashley Nguyen <ashley.nguyen27@berkeley.edu>
2017-10-08 06:28:27 -0700

    front-end

--- client/main.js
+++ client/main.js
@@ -6,31 +6,18 @@ import './main.html';
 Tasks = new Mongo.Collection('tasks');
 Events = new Mongo.Collection('events');
 
-// Get the navbar
 
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  var navbar = document.getElementById("navbar");
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 Template.homePage.helpers({
     tasks: function() {
         return Tasks.find();
-    }, 
+    },
 });
 
 Template.poolPage.helpers({
     tasks: function() {
         return Tasks.find();
-    }, 
+    },
 });
 
 Template.homePage.events({
