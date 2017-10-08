f1994e5d391d5e38b0815ee5bf77af135321f6fd
Tian Qin <tianqin@Tians-MacBook-Pro.local>
2017-10-08 07:10:59 -0700

    FINAL VERSION WORKING

--- client/main.js
+++ client/main.js
@@ -6,20 +6,12 @@ import './main.html';
 Tasks = new Mongo.Collection('tasks');
 Events = new Mongo.Collection('events');
 
-// Get the navbar
+contractAddr = '0x8A3A405D897E665CF7aACDd8bF4be47dD96C6e05';
+abi = [{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdraw","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"pushRequest","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"string"}],"name":"downVote","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"balanceCheck","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalFundGetter","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"string"},{"name":"supportersWin","type":"bool"}],"name":"settle","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"destroy","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"approveAccount","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"string"}],"name":"upVote","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"string"}],"name":"pushTask","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_voter","type":"address"}],"name":"Vote","type":"event"}];
+var myContract = web3.eth.contract(abi);
+var ctr = myContract.at(contractAddr);
 
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  var navbar = document.getElementById("navbar");
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+oneEther = web3.toWei(1, "ether");
 
 Template.homePage.helpers({
     tasks: function() {
@@ -30,7 +22,46 @@ Template.homePage.helpers({
 Template.poolPage.helpers({
     tasks: function() {
         return Tasks.find();
-    }, 
+    },
+    totalFund: function() {
+        ctr.totalFund().call(function(err, res) {
+            return web3.fromWei(res, 'ether');
+        });
+    },
+});
+
+Template.poolPage.events({
+    'click .upvote': function() {
+        var id = this._id;
+        var u = this.upvotes;
+        ctr.vote(function(err, res) {
+            if (res) {
+                ctr.upVote(id, {value: oneEther}, 
+                    function(err, res) {
+                        Tasks.update(id, {$set: {upvotes: u+1}});
+                    }
+                );
+            } else {
+                alert("Your account is not approved.\nApprove by depositing ether");
+            }
+        });
+    },
+    'click .downvote': function() {
+        var id = this._id;
+        var d = this.downvotes;
+        ctr.vote(function(err, res) {
+            if (res) {
+                ctr.downVote(id, {value: oneEther}, 
+                    function(err, res) {
+                        Tasks.update(id, {$set: {downvotes: d+1}});
+                    }
+                );
+            } else {
+                alert("Your account is not approved.\nApprove by depositing ether");
+            }
+        });
+    },
+
 });
 
 Template.homePage.events({
@@ -52,18 +83,68 @@ Template.homePage.events({
         }
         return false;
     },
-    'click .task': function(event) {
+    'click .complete': function(event) {
         var id = this._id;
         var c = Tasks.findOne({_id: id}).completed;
         console.log("c is " + c);
-        Tasks.update(id, {$set: {completed: !c}}, function(err, res) {
+        ctr.settle(id, true, function(err, res) {
             if (err) {
-                console.log("errored");
+                console.log("Could not settle. Error is " + err);
             } else {
-                console.log("c now is " + !c);
+                Tasks.update(id, {$set: {completed: !c}}, function(err, res) {
+                    if (err) {
+                        console.log("errored");
+                    } else {
+                        console.log("c now is " + !c);
+                    }
+                });
             }
         });
     },
+    'submit .send-ether': function(event) {
+        var amount = event.target.etherAmount.value;
+        weiAmount = web3.toWei(amount, "ether");
+        if (amount <= 0) {
+            alert("Please specify amount of ether");
+        } else {
+            ctr.approveAccount({value: weiAmount}, 
+                function(err, res) {
+                    if (res) {
+                        console.log("account approved");
+                    } else {
+                        console.log("error when approving account");
+                    }
+                });
+            event.target.etherAmount.value = "";
+        }
+        return false;
+    }, 
+    'submit .withdraw-ether': function(event) {
+        var amount = event.target.etherAmount.value;
+        var weiAmount = web3.toWei(amount, "ether")
+        if (amount <= 0) {
+            alert("Please specify positive amount of ether");
+        } else {
+            ctr.withdraw(weiAmount,
+                function(err, res) {
+                    if (res) {
+                        console.log("success");
+                    } else {
+                        ctr.balanceCheck.call(
+                            function(err, res) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    balance = web3.fromWei(res, 'ether');
+                                    alert("You have " + balance + " ether deposited");
+                                }
+                        });
+                    } 
+                });
+            event.target.etherAmount.value = "";
+        }
+        return false;
+    }, 
 });
 
 Template.task.events({
@@ -71,12 +152,29 @@ Template.task.events({
         Tasks.remove(this._id);
     },
     'click .push': function(event) {
-        Tasks.update(this._id, {$set: {notPushed: false}}, function(err, res) {
-            if (err) {
-                console.log("errored");
-            } else {
-                console.log("Task pushed");
+        var id = this._id;
+        ctr.pushRequest.call(
+            function(err, res) {
+                console.log("push request processed");
+                if (res) {
+                    ctr.pushTask(id, {value: oneEther},
+                        function(err, res) {
+                            if (res) {
+                                Tasks.update(id, {$set: {notPushed: false}}, function(err, res) {
+                                    if (err) {
+                                        console.log("errored");
+                                    } else {
+                                        console.log("Task pushed");
+                                    }
+                                });
+                            }
+                        }
+                    );
+                } else {
+                    alert("Push to Pool needs account approval.\nApprove by depositing ether");
+                }
             }
-        });
+        );
     },
+
 });
