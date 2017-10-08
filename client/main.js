b2f20d8188a1bfc4bcbf8e3978bf43821e0f0011
Tian Qin <tianqin@airbears2-10-142-65-47.airbears2.1918.berkeley.edu>
2017-10-07 18:46:24 -0700

    no css

--- client/main.js
+++ client/main.js
@@ -6,3 +6,31 @@ import './main.html';
 Tasks = new Mongo.Collection('tasks');
 Events = new Mongo.Collection('events');
 
+Template.body.helpers({
+    tasks: function() {
+        return Tasks.find();
+    }, 
+});
+
+Template.body.events({
+    'submit .new-task': function(event) {
+        console.log("new task submitted");
+        var body = event.target.body.value;
+        if (body == "" ) {
+            alert("Task cannot be empty");
+        } else {
+            console.log("new task inserting");
+            Tasks.insert({
+                body: body,
+                completed: false,
+                upvotes: 0,
+                downvotes: 0
+            });
+        }
+        return false;
+    },
+});
+
+Template.task.events({
+
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+Tasks = new Mongo.Collection('tasks');
+Events = new Mongo.Collection('events');
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
